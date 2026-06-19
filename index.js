a6407c3231d610991ebc40483f14811bfee7e01e
mrudula <mrudulakanduri@gmail.com>
2026-06-19 22:09:25 +0530

    added edit and delete routes

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing");
 const path = require("path");
+const methodoverride=require("method-override");
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
+app.use(methodoverride("_method"));
+
 
 
 main().then(()=>{
@@ -24,11 +27,41 @@ app.get("/listings",async(req,res)=>{
     res.render("listings/index.ejs",{alllists});
 });
 
+app.get("/listings/new", (req,res)=>{
+    res.render("listings/new.ejs");
+});
+
 app.get("/listings/:id",async(req,res)=>{
     let {id} = req.params;
     const list = await Listing.findById(id);
     res.render("listings/show.ejs",{list});
 
+});
+
+app.post("/listings",async(req,res)=>{
+    let {listing} = req.body;
+    let newlisting = new Listing(listing);
+    await newlisting.save();
+    res.redirect("/listings");
+});
+
+app.get("/listings/:id/edit",async(req,res)=>{
+    let {id} = req.params;
+    const list = await Listing.findById(id);
+    res.render("listings/edit.ejs",{list});
+   
+});
+
+app.put("/listings/:id",async(req,res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id,req.body.listing);
+    res.redirect(`/listings/${id}`);
+});
+
+app.delete("/listings/:id",async(req,res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
 });
 
 app.get("/",(req,res)=>{

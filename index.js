2a01db8a02643938607ca4114d56b05622f0f608
mrudula <mrudulakanduri@gmail.com>
2026-06-19 13:32:23 +0530

    added two routed

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing = require("./models/listing");
+const path = require("path");
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}));
 
 
 main().then(()=>{
@@ -12,7 +17,20 @@ main().then(()=>{
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/Wanderlust');
 }
- 
+
+
+app.get("/listings",async(req,res)=>{
+    const alllists=await Listing.find({})
+    res.render("listings/index.ejs",{alllists});
+});
+
+app.get("/listings/:id",async(req,res)=>{
+    let {id} = req.params;
+    const list = await Listing.findById(id);
+    res.render("listings/show.ejs",{list});
+
+});
+
 app.get("/",(req,res)=>{
     res.send("Root is Working");
 });

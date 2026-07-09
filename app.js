2e11958cf382f97e046f364f5f3da9a1f6d1ce83
mrudula <mrudulakanduri@gmail.com>
2026-07-09 19:56:15 +0530

    Restore correct app.js with MongoDB Atlas connection

--- app.js
+++ app.js
@@ -1,30 +1,48 @@
+if (process.env.NODE_ENV != "production") {
+    require("dotenv").config();
+}
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
-const methodoverride=require("method-override");
+const methodoverride = require("method-override");
 const ejsmate = require("ejs-mate");
 const expresserror = require("./utils/expresserror.js");
 const session = require("express-session");
+const Listing = require("./models/listing.js");
 
 const MongoStore = require("connect-mongo");
 
 const flash = require("connect-flash");
-const passport=require("passport");
+const passport = require("passport");
 const Localstrategy = require("passport-local").Strategy;
 const User = require("./models/user.js");
 
-const sessionOptions={
-    secret : "mysecretsuperkey",
-    resave:false,
-    saveUninitialized : true,
-    cookie:{
-        expires:Date.now() + 7 * 24 *60*60*1000,
-        maxAge:7 * 24 *60*60*1000,
-        httpOnly : true
+const dbUrl = process.env.ATLASDB_URL;
 
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    crypto: {
+        secret: "mysecretkey",
     },
-    
+    touchAfter: 24 * 3600,
+});
+
+store.on("error", (err) => {
+    console.log("ERROR IN MONGO SESSION STORE", err);
+});
+
+const sessionOptions = {
+    store,
+    secret: "mysecretkey",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+    },
 };
 
 app.use(session(sessionOptions));
@@ -32,71 +50,64 @@ app.use(flash());
 
 app.use(passport.initialize());
 app.use(passport.session());
+
 passport.use(new Localstrategy(User.authenticate()));
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use((req,res,next)=>{
-    res.locals.success =  req.flash("success");
-    res.locals.error =  req.flash("error");
+app.use((req, res, next) => {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
     res.locals.currUser = req.user;
     next();
 });
- 
+
 const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/reviews.js");
 const userRouter = require("./routes/users.js");
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodoverride("_method"));
-app.use(express.static(path.join(__dirname,"/public")));
+app.use(express.static(path.join(__dirname, "/public")));
 
-app.engine("ejs",ejsmate);
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"views"));
+app.engine("ejs", ejsmate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-
-
-
-main().then(()=>{
-    console.log("connection sucessful");
-}).catch((err)=>{
-    console.log(err);
-});
+main()
+    .then(() => {
+        console.log("connection successful");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/Wanderlust');
+    await mongoose.connect(dbUrl);
 }
 
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
 
-
-app.use("/listings",listingRouter);
-app.use("/listings/:id/reviews",reviewRouter);
-app.use("/",userRouter);
-
-
- 
-
-app.all("*splat",(req,res,next)=>{
-    next(new expresserror(404,"page not found!!"));
+app.get("/", async (req, res) => {
+    const alllists = await Listing.find({});
+    res.render("listings/index.ejs", { alllists });
 });
 
+app.all("*splat", (req, res, next) => {
+    next(new expresserror(404, "page not found!!"));
+});
 
 app.use((err, req, res, next) => {
-    console.error(err);          // Print the full error
-    console.error(err.stack);    // Print the stack trace
+    console.error(err);
+    console.error(err.stack);
 
     let { status = 500, message = "Something went wrong" } = err;
     res.status(status).render("listings/error.ejs", { message });
 });
 
-// app.use((err, req, res, next) => {
-//     let { status = 500, message = "something went wrong" } = err;
-//     res.render("listings/error.ejs",{message});
-// });
-
-app.listen(8080,()=>{
+app.listen(8080, () => {
     console.log("app is listening on the port 8080");
 });
-
